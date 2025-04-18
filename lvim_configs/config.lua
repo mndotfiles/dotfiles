03db21d80bda2e7c6cd70939b12eae178ee48a04
Marija Ferber <marija.ferber@spoton.com>
2025-04-18 17:30:31 -0500

    getting to normal

--- lvim_configs/config.lua
+++ lvim_configs/config.lua
@@ -8,41 +8,15 @@
 lvim.plugins = {
   {
   "folke/tokyonight.nvim",
-  "dracula/vim",
-  "ChristianChiarulli/swenv.nvim",
-  "stevearc/dressing.nvim",
   "mfussenegger/nvim-dap-python",
-  "rcarriga/nvim-dap-ui",
   "nvim-neotest/neotest",
+  "nvim-neotest/nvim-nio",
   "nvim-neotest/neotest-python"},
-{
-  "echasnovski/mini.map",
-  branch = "stable",
-  config = function()
-    require('mini.map').setup()
-    local map = require('mini.map')
-    map.setup({
-      integrations = {
-        map.gen_integration.builtin_search(),
-        map.gen_integration.diagnostic({
-          error = 'DiagnosticFloatingError',
-          warn  = 'DiagnosticFloatingWarn',
-          info  = 'DiagnosticFloatingInfo',
-          hint  = 'DiagnosticFloatingHint',
-        }),
-      },
-      symbols = {
-        encode = map.gen_encode_symbols.dot('4x2'),
-      },
-      window = {
-        side = 'right',
-        width = 20, -- set to 1 for a pure scrollbar :)
-        winblend = 15,
-        show_integration_count = false,
-      },
-    })
-  end
-}
+  -- "ChristianChiarulli/swenv.nvim",
+  "aserowy/tmux.nvim",
+  -- Legacy
+  "preservim/vimux",
+  "janko/vim-test",
 }
 
 lvim.colorscheme = "tokyonight-night"
@@ -50,6 +24,15 @@ lvim.colorscheme = "tokyonight-night"
 lvim.builtin.treesitter.ensure_installed = {
   "python",
 }
+
+-- setup debug adapter
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+vim.g["test#strategy"] = "vimux"
 
 -- setup formatting
 local formatters = require "lvim.lsp.null-ls.formatters"
@@ -61,12 +44,6 @@ lvim.format_on_save.pattern = { "*.py" }
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { command = "flake8", args = { "--ignore=E203,F812,H101,H202,H233,H301,H306,H401,H403,H404,H405,H501 --max-complexity=18 --select=B,C,E,F,W,T4,B9" }, filetypes = { "python" } } }
 
--- setup debug adapter
-lvim.builtin.dap.active = true
-local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
-pcall(function()
-  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
-end)
 
 -- setup testing
 require("neotest").setup({
@@ -94,8 +71,19 @@ lvim.builtin.which_key.mappings["dF"] = {
   "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Test Class DAP" }
 lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
 
+lvim.builtin.which_key.mappings["v"] = {
+  name = "Vimux",
+  p = { ":VimuxPromptCommand<cr>", "Run vimux"},
+}
 
--- binding for switching
+lvim.builtin.which_key.mappings["r"] = {
+  name = "Vimux Tests",
+  l = { ":VimuxRunLastCommand<cr>", "Run last command" },
+  b = { ":TestFile<cr>", "Run test buffer" },
+  f = { ":TestNearest<cr>", "Run nearest file" },
+}
+
+-- -- binding for switching
 lvim.builtin.which_key.mappings["C"] = {
   name = "Python",
   c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
